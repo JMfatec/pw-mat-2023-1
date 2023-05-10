06534527e4e14f5af89cfdd5e85cc0b194beeaa3
João Marcos <124882613+JMfatec@users.noreply.github.com>
2023-05-10 14:03:22 +0000

    (10/04) Modo escuro e FooterBar

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -4,6 +4,10 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import TopBar from './components/ui/TopBar'
+import theme from './utils/theme'
+import { ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box'
+import FooterBar from './components/ui/FooterBar';
 //import './App.css'
 
 function App() {
@@ -11,7 +15,16 @@ function App() {
   return (
     <>
      <BrowserRouter>
-     <TopBar />
+     <ThemeProvider theme={theme}>
+       <Box sx={{ 
+        width: '100vw' , 
+        height: '100vh', 
+        backgroundColor: 'background.default'
+      }}>
+       <TopBar />
+       <FooterBar />
+       </Box>
+     </ThemeProvider>
      </BrowserRouter>
     </>
   )
