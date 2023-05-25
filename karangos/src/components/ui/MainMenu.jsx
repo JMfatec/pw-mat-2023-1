0834d49d53a383042736c247add8518b805eaee2
João Marcos <124882613+JMfatec@users.noreply.github.com>
2023-05-25 12:12:28 +0000

    (25/05) Continuação da listagem de clientes

--- karangos/src/components/ui/MainMenu.jsx
+++ karangos/src/components/ui/MainMenu.jsx
@@ -6,7 +6,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom'
 import Divider from '@mui/material/Divider'
 
-export default function BasicMenu() {
+export default function MainMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -18,21 +18,20 @@ export default function BasicMenu() {
 
   return (
     <div>
-      
       <IconButton 
-      id="basic-button"
-      aria-controls={open ? 'MainMenu' : undefined}
-      aria-haspopup="true"
-      aria-expanded={open ? 'true' : undefined}
-      onClick={handleClick}
-      edge="start" 
-      color="inherit" 
-      aria-label="menu" 
-      sx={{ mr: 2 }}
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        edge="start" 
+        color="inherit" 
+        aria-label="menu" 
+        sx={{ mr: 2 }}
       >
-            <MenuIcon />
-          </IconButton>
-
+        <MenuIcon />
+      </IconButton>
+      
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
@@ -42,26 +41,22 @@ export default function BasicMenu() {
           'aria-labelledby': 'basic-button',
         }}
       >
-
         <MenuItem divider
-         onClick={handleClose}
-         component={Link}>
-         to="/"
-        Página inicial
-        </MenuItem>    
-
-         <MenuItem divider
-         onClick={handleClose}
-         component={Link}>
-         to="/"
-         Clientes
+          onClick={handleClose}
+          component={Link}
+          to="/"
+        >
+          Página inicial
         </MenuItem>
         
-       
-         <Link to="/customers">
-         <MenuItem onClick={handleClose}>Clientes</MenuItem>
-         </Link>
-        
+        <MenuItem 
+          onClick={handleClose}
+          component={Link}
+          to="/customers"
+        >
+          Clientes
+        </MenuItem>
+
       </Menu>
     </div>
   );
--- karangos/src/pages/CustomersList.jsx
+++ karangos/src/pages/CustomersList.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import Typography from "@mui/material/Typography"
-import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import { DataGrid } from '@mui/x-data-grid';
+import { format } from 'date-fns'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import  IconButton  from "@mui/material/IconButton";
 
 export default function customersList(){
 
@@ -39,56 +43,82 @@ async function LoadData(){
 const columns = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
-      field: 'firstName',
-      headerName: 'First name',
+      field: 'name',
+      headerName: 'Nome',
+      width: 300,
+    },
+    {
+      field: 'ident_document',
+      headerName: 'CPF',
+      align: 'center',
+      headerAlign: 'center',
       width: 150,
-      editable: true,
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
+      field: 'birth_date',
+      headerName: 'Data_nasc',
+      align: 'center',
+      headerAlign: 'center',
+      width: 100,
+      valueFormatter: params => { 
+        if(params.value) return format(new Date(params?.value), 'dd/MM/yyyy')
+           else return ''
+       }
+    },
+    {
+      field: 'city',
+      headerName: 'Município/UF',
+      width: 300,
+      // Colocando dois campos na mesma célula
+      valueGetter: params => params.row.city + '/' + params.row.uf,
+    },
+    {
+      field: 'phone',
+      headerName: 'Celular',
+      align: 'center',
+      headerAlign: 'center',
       width: 150,
-      editable: true,
     },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 110,
-      editable: true,
+      field: 'email',
+      headerName: 'E-mail',
+      width: 200,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      field: 'edit',
+      headerName: 'Editar',
+      align: 'center',
+      headerAlign: 'center',
+      width: 90,
+      renderCell: params => 
+      <IconButton aria-label='Editar'>
+        <EditIcon />
+      </IconButton>
     },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    {
+      field: 'delete',
+      headerName: 'Excluir',
+      align: 'center',
+      headerAlign: 'center',
+      width: 90,
+      renderCell: params =>
+      <IconButton aria-label='Excluir'>
+      <DeleteForeverIcon color='error' />
+      </IconButton>
+
+    },
   ];
   
     return (
 
         <>
-        <Typography variant="h1">
+        <Typography variant="h1" sx={{ mb: '50px'}}>
             Listagem de clientes
         </Typography>
-        <Box sx={{ height: 400, width: '100%' }}>
+
+        <Paper elevation={4} sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={customers}
         columns={columns}
         initialState={{
           pagination: {
@@ -101,10 +131,8 @@ const columns = [
         checkboxSelection
         disableRowSelectionOnClick
       />
-    </Box>
-        <div>
-            { JSON.stringify(customers) }
-        </div>
+    </Paper>
+        
         </>
     )
 }
